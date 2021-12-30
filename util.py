19d325ac7a6f6545747421a747ebf70ccf536f21
Timofey Danshin <t.danshin@gmail.com>
2021-12-30 12:04:58 +0300

    Apply certain candidates to the board

--- util.py
+++ util.py
@@ -2,6 +2,13 @@ def row_indexes(n):
     start = n * 9
     return list(range(start, start + 9))
 
+
 def column_indexes(n):
     return [x * 9 + n for x in range(9)]
 
+
+def apply_cands_to_board(board, candidates):
+    for i, cands in enumerate(candidates):
+        if len(cands) == 1:
+            board[i] = cands[0]
+    return board
